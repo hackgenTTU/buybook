8a61dc0332834330acde2355356c70a5511c762c
linroex <linroex@coder.tw>
2013-10-20 02:21:39 +0800

    clean module and control

--- control/load_module.php
+++
@@ -1,6 +0,0 @@
-<?php
-    function autoload($name){
-        spl_autoload('../module/' . strtolower($name));
-    }
-    spl_autoload_register('autoload');
-?>
--- control/sql.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con_str = "mysql:host=localhost;dbname=buybook";
-    $db = new PDO($con_str,'buybook','5Lz9w8xZGEmfxafN');
-<<<<<<< HEAD
-    $db->exec("set names utf8");
-=======
-    
->>>>>>> a125c070b88e1279288cce33935d8dd9afbcd3bd
-?>
