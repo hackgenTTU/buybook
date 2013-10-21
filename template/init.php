8250abadfe3c49615c6f01bb0c552ae04be7a057
linroex <linroex@coder.tw>
2013-10-21 14:00:30 +0800

    new view init

--- template/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    
-    if(@isset($_SESSION['msg'])){
-        echo '<script>alert("' . $_SESSION['msg'] . '")</script>';
-        $_SESSION['msg'] = null;
-    }
-
-?>
